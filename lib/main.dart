dec130b88a819802ba5b21231a5435f8808f37d9
Raffaele Intorcia <raffaele.intorcia@cgn.it>
2024-02-02 17:41:34 +0100

    Full screen widget with lorm ipsum

--- lib/main.dart
+++ lib/main.dart
@@ -31,30 +31,41 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(
-          title: 'Italian Proverbs Badly Translated Home Page'),
+      home: const MyHomePage(title: 'Italian Proverbs Badly Translated'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Container(
+        margin: EdgeInsets.all(10),
+        // color: Colors.deepPurple,
+        child: Center(
+          child: const Row(
+            children: [
+              Expanded(
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in leo sed sapien lobortis cursus. Pellentesque hendrerit dui eget volutpat iaculis.',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
+/*
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -124,3 +135,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
